5252643f0f6833c632ab0b303e080d88b22966da
Aïmen <aimen2nice@gmail.com>
2025-01-28 14:05:05 +0100

    table user done

--- back/src/config/database.js
+++ back/src/config/database.js
@@ -4,10 +4,19 @@ const db = new Database('mydb.db');
 
 db.exec(`CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
+  firstname VARCHAR(50),
+  lastname VARCHAR(50),   
   email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL,
-  name VARCHAR(255),
+  address VARCHAR(300),
+  zip VARCHAR(6),
+  city VARCHAR(50),   
+  phone VARCHAR(20),
+  role VARCHAR(20),
   verified BOOLEAN DEFAULT FALSE,
+    created_at DATETIME,
+    updated_at DATETIME,
+    deleted_at DATETIME,  
   reset_token VARCHAR(255))`);
 
 export default db;
