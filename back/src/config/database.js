6f935547f1285200238066597116e1d40c2600db
Moreira_Stephane <moreira.s@outlook.com>
2025-01-29 16:36:07 +0100

    feat table request

--- back/src/config/database.js
+++ back/src/config/database.js
@@ -14,10 +14,12 @@ db.exec(`CREATE TABLE IF NOT EXISTS users (
   phone VARCHAR(20),
   role VARCHAR(20) NOT NULL,
   verified BOOLEAN DEFAULT FALSE,
-    created_at DATETIME,
-    updated_at DATETIME,
-    deleted_at DATETIME,  
-  reset_token VARCHAR(255))`);
+  created_at DATETIME,
+  updated_at DATETIME,
+  deleted_at DATETIME,  
+  reset_token VARCHAR(255)
+  )`
+);
 
 db.exec(`CREATE TABLE IF NOT EXISTS organisation (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -31,11 +33,34 @@ db.exec(`CREATE TABLE IF NOT EXISTS organisation (
   mail VARCHAR(20)NOT NULL,
   phone VARCHAR(20) NOT NULL,
   repas_max int ,
+  description VARCHAR(255) ,
+  image VARCHAR(255),
+  menu VARCHAR(255),  
+  created_at DATETIME,
+  updated_at DATETIME,
+  deleted_at DATETIME)`);
+
+db.exec(
+  `CREATE TABLE IF NOT EXISTS request (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(50)NOT NULL,
+    address VARCHAR(255)NOT NULL,   
+    zip VARCHAR(50) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    siret VARCHAR(50) NOT NULL,
+    type VARCHAR(6) NOT NULL,
+    contact VARCHAR(50) NOT NULL,   
+    mail VARCHAR(50) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    repas_max int ,
     description VARCHAR(255) ,
     image VARCHAR(255),
     menu VARCHAR(255),  
     created_at DATETIME,
-  updated_at DATETIME,
-  deleted_at DATETIME)`);
+    updated_at DATETIME,
+    deleted_at DATETIME 
+    )
+  `
+)
 
 export default db;
