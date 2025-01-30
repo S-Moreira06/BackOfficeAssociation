bbb3a5a4ac7232f847c62da1f1845de887a8c4e1
Hugodesv <hugo.desvignes@laplateforme.io>
2025-01-30 12:07:20 +0100

    table beneficiaire
    J'ai ajouté tout les champs de la table changer le nombre de caractère max pour des champs certains des booleans ou des dates times

--- back/src/config/database.js
+++ back/src/config/database.js
@@ -1,6 +1,6 @@
-import Database from 'libsql';
+import Database from "libsql";
 
-const db = new Database('mydb.db');
+const db = new Database("mydb.db");
 
 db.exec(`CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -8,6 +8,21 @@ db.exec(`CREATE TABLE IF NOT EXISTS users (
   password VARCHAR(255) NOT NULL,
   name VARCHAR(255),
   verified BOOLEAN DEFAULT FALSE,
-  reset_token VARCHAR(255))`);
+  reset_token VARCHAR(255))
+  
+  CREATE TABLE IF NOT EXISTS beneficiary (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    firstname VARCHAR(255) UNIQUE NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    adress VARCHAR(255),
+    zip VARCHAR(6),
+    city VARCHAR(50), 
+    phone VARCHAR(50),
+    remark VARCHAR(255),
+    rgpd BOOLEAN DEFAULT FALSE,
+    created_at DATETIME,
+    modified_at DATETIME,
+    deleted_at DATETIME
+  );`);
 
 export default db;
