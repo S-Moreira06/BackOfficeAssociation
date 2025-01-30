1f882a9f5c0b9a00c5aacfef3e6ca49a39a3dcee
Hugodesv <hugo.desvignes@laplateforme.io>
2025-01-30 13:58:27 +0100

    Update database.js

--- back/src/config/database.js
+++ back/src/config/database.js
@@ -8,21 +8,22 @@ db.exec(`CREATE TABLE IF NOT EXISTS users (
   password VARCHAR(255) NOT NULL,
   name VARCHAR(255),
   verified BOOLEAN DEFAULT FALSE,
-  reset_token VARCHAR(255))
-  
-  CREATE TABLE IF NOT EXISTS beneficiary (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    firstname VARCHAR(255) UNIQUE NOT NULL,
-    lastname VARCHAR(255) NOT NULL,
-    adress VARCHAR(255),
-    zip VARCHAR(6),
-    city VARCHAR(50), 
-    phone VARCHAR(50),
-    remark VARCHAR(255),
-    rgpd BOOLEAN DEFAULT FALSE,
-    created_at DATETIME,
-    modified_at DATETIME,
-    deleted_at DATETIME
-  );`);
+  reset_token VARCHAR(255)
+);`);
+
+db.exec(`CREATE TABLE IF NOT EXISTS beneficiary (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  firstname VARCHAR(255) UNIQUE NOT NULL,
+  lastname VARCHAR(255) NOT NULL,
+  adress VARCHAR(255),
+  zip VARCHAR(6),
+  city VARCHAR(50), 
+  phone VARCHAR(50),
+  remark VARCHAR(255),
+  rgpd BOOLEAN DEFAULT FALSE,
+  created_at DATETIME,
+  modified_at DATETIME,
+  deleted_at DATETIME
+);`);
 
 export default db;
