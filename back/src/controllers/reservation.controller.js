48effa4f2fd863f252c76d252080f2f5fa3ddf84
Moreira_Stephane <moreira.s@outlook.com>
2025-02-04 14:15:21 +0100

    add controller to add reservation

---
+++ back/src/controllers/reservation.controller.js
@@ -0,0 +1,16 @@
+import reservationService from '../services/reservation.service';
+
+async function createReservation(c) {
+    try {
+        const data = c.req.valid('json')
+            await reservationService.createReservation(data)
+            return c.json({
+              message: 'Votre reservation a bien été envoyée. Le restaurateur confirmera ces repas les plus brefs delais.'
+            }, 201)
+    } catch (error) {
+        console.error(error)
+        return c.json({ error : 'request failed'}, 400)
+    }
+}
+
+export {createReservation}
