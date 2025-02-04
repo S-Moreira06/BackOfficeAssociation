09eec657daf4eef979c320f7c5c085acd299a79c
Moreira_Stephane <moreira.s@outlook.com>
2025-02-04 14:05:13 +0100

    add service to add reservation

---
+++ back/src/services/reservation.service.js
@@ -0,0 +1,14 @@
+import db from '../config/database.js';
+
+async function createReservation(data) {
+    const query = `
+    INSERT INTO request (id_organisation, id_availability, time, email, nb_place_setting, status, take_away, commentary)
+    VALUES (?,?,?,?,?,?,?,?)
+  `;
+  const values = [data.id_organisation,data.id_availability,data.time,data.email, data.nb_place_setting, data.status, data.take_away, data.commentary];
+  
+  const result = await db.prepare(query).run(values);
+  return await db.prepare('SELECT * FROM reservation WHERE id = ?').get(result.lastInsertRowid);
+}
+
+export default {createReservation};
