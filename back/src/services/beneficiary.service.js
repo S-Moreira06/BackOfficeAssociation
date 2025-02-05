d3458e2ff83c2d0daafd11a436195fda1007e973
Moreira_Stephane <moreira.s@outlook.com>
2025-02-05 14:28:52 +0100

    add beneficiary services

---
+++ back/src/services/beneficiary.service.js
@@ -0,0 +1,14 @@
+import db from '../config/database.js'
+
+async function createBeneficiary(data) {
+    const query = `
+      INSERT INTO beneficiary (firstname, lastname, address, zip, city, phone, remark, rgpd)
+      VALUES (?,?,?,?,?,?,?,?)
+    `;
+    const values = [data.firstname,data.lastname,data.address,data.zip,data.city, data.phone,data.remark, data.rgpd];
+    
+    const result = await db.prepare(query).run(values);
+    return await db.prepare('SELECT * FROM beneficiary WHERE id = ?').get(result.lastInsertRowid); 
+  }
+
+  export default {createBeneficiary}
