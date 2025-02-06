896448d5e0c676c1ddb3332e6d007a3964c9e9b5
Moreira_Stephane <moreira.s@outlook.com>
2025-02-06 16:00:33 +0100

    add request migration

--- back/src/config/database.js
+++ back/src/config/database.js
@@ -22,53 +22,6 @@ db.exec(
     reset_token VARCHAR(255)
   )
   `);
-
-// db.exec(`
-//   CREATE TABLE IF NOT EXISTS organisation (
-//     id INTEGER PRIMARY KEY AUTOINCREMENT,
-//     name VARCHAR(50)NOT NULL,
-//     address VARCHAR(255)NOT NULL,   
-//     zip VARCHAR(6)  NOT NULL,
-//     city VARCHAR(50) NOT NULL,
-//     siret VARCHAR(50)NOT NULL,
-//     type VARCHAR(6)NOT NULL,
-//     contact VARCHAR(50)NOT NULL,   
-//     email VARCHAR(255)NOT NULL,
-//     phone VARCHAR(20) NOT NULL,
-//     max_meal INTEGER ,
-//     description VARCHAR(255) ,
-//     image VARCHAR(255),
-//     menu VARCHAR(255),  
-//     is_archived BOOLEAN DEFAULT FALSE,
-//     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-//     updated_at DATETIME,
-//     deleted_at DATETIME
-//   )
-//   `);
-
-// db.exec(
-//   `CREATE TABLE IF NOT EXISTS request (
-//     id INTEGER PRIMARY KEY AUTOINCREMENT,
-//     name VARCHAR(50)NOT NULL,
-//     address VARCHAR(255)NOT NULL,   
-//     zip VARCHAR(6) NOT NULL,
-//     city VARCHAR(50) NOT NULL,
-//     siret VARCHAR(50) NOT NULL,
-//     type VARCHAR(6) NOT NULL,
-//     contact VARCHAR(50) NOT NULL,   
-//     email VARCHAR(255) NOT NULL,
-//     phone VARCHAR(20) NOT NULL,
-//     max_meal INTEGER ,
-//     description VARCHAR(255) ,
-//     image VARCHAR(255),
-//     menu VARCHAR(255),  
-//     is_archived BOOLEAN DEFAULT FALSE,
-//     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-//     updated_at DATETIME,
-//     deleted_at DATETIME 
-//   )
-//   `);
-
 // db.exec(
 //     `CREATE TABLE IF NOT EXISTS beneficiary (
 //           id INTEGER PRIMARY KEY AUTOINCREMENT,
