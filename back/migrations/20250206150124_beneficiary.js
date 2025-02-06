f9f4c7299fdea7270945d9d2f46ff0c8440174c7
Moreira_Stephane <moreira.s@outlook.com>
2025-02-06 16:37:18 +0100

    add beneficiary migration

---
+++ back/migrations/20250206150124_beneficiary.js
@@ -0,0 +1,34 @@
+/**
+* Migrates the database schema upward, making changes to bring the schema toward the latest version.
+* @param client - The libsql client to use when migrating.
+* @returns { Promise<void> }
+*/
+export async function up(client) {
+    await client.execute(
+        `CREATE TABLE IF NOT EXISTS beneficiary (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          firstname VARCHAR(255) NOT NULL,
+          lastname VARCHAR(255) NOT NULL,
+          address VARCHAR(255) NOT NULL,
+          zip VARCHAR(6) NOT NULL,
+          city VARCHAR(50) NOT NULL, 
+          phone VARCHAR(50) NOT NULL,
+          remark VARCHAR(255),
+          rgpd BOOLEAN DEFAULT FALSE,
+          is_archived BOOLEAN DEFAULT FALSE,
+          created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+          updated_at DATETIME,
+          deleted_at DATETIME
+        )`
+    );
+}
+
+/**
+* Migrates the database schema downward, making changes to roll the schema back to a previous version.
+* @param client - The libsql client to use when migrating.
+* @returns { Promise<void> }
+*/
+export async function down(client) {
+        await client.execute("DROP TABLE organisation;");
+
+}
--- back/src/config/database.js
+++ back/src/config/database.js
@@ -22,22 +22,5 @@ db.exec(
     reset_token VARCHAR(255)
   )
   `);
-// db.exec(
-//     `CREATE TABLE IF NOT EXISTS beneficiary (
-//           id INTEGER PRIMARY KEY AUTOINCREMENT,
-//           firstname VARCHAR(255) UNIQUE NOT NULL,
-//           lastname VARCHAR(255) NOT NULL,
-//           adress VARCHAR(255),
-//           zip VARCHAR(6),
-//           city VARCHAR(50), 
-//           phone VARCHAR(50),
-//           remark VARCHAR(255),
-//           rgpd BOOLEAN DEFAULT FALSE,
-//           created_at DATETIME,
-//           modified_at DATETIME,
-//           deleted_at DATETIME
-//     )`
-// );
-
 
 export default db;
