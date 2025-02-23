7f73f4413d8cbce77009c6cd80b29a214c59668c
Moreira_Stephane <moreira.s@outlook.com>
2025-02-24 00:04:18 +0100

    fix home

--- front/src/page/home.jsx
+++ front/src/page/home.jsx
@@ -1,16 +1,34 @@
 import { useQuery } from '@tanstack/react-query'
 import React, { useEffect } from 'react'
+import { useNavigate } from "react-router-dom";
 
+import { 
+  Card, 
+  CardContent, 
+  CardDescription, 
+  CardFooter, 
+  CardHeader, 
+  CardTitle 
+} from "@/components/ui/card";
+
+import { getAllUsers } from '@/api/user'
 
 export default function Home() {
-  
+  const { isPending, isError, data, error } = useQuery({ queryKey: ['usersList'], queryFn: getAllUsers })
 
   const storedData = localStorage.getItem("accessToken")
+  const navigate = useNavigate()
   
+
   return (
     <div className='px-20 py-5 min-h-screen'>
       {storedData ? (
-        <p>Vous êtes connectez</p>
+        <>
+        <Card  onClick={() => navigate("/UsersList")}>
+          <CardHeader>Nombre d'utilisateurs</CardHeader>
+          <CardContent>{data?.users.length}</CardContent>
+        </Card>
+        </>
       ):(
         <>
         <h1>LA PETITE LILI</h1>
