9e987db85c6ce6fcde946f6473f70d66b75a103a
Aïmen <aimen2nice@gmail.com>
2025-02-23 21:09:02 +0100

    seed ok

---
+++ back/seeds/optionOrganisation.js
@@ -0,0 +1,21 @@
+/**
+ * Seeds the database with preset data.
+ * @param client - The libsql client to use when migrating.
+ * @returns { Promise<void> }
+ */
+export async function seed(client) {
+    await client.execute("DELETE FROM option_organisation;");
+    await client.execute(`INSERT INTO option_organisation (id_organisation, id_restaurant)
+VALUES
+    (3, 7),
+    (1, 5),
+    (2, 9),
+    (4, 2),
+    (5, 1),
+    (6, 10),
+    (7, 3),
+    (8, 4),
+    (9, 6),
+    (10, 8)
+`);
+}
