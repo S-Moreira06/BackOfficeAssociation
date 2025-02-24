e406be0373a8708b1e844ddf79e95033ba142334
Noura <nourallahbejaoui@gmail.com>
2025-02-24 14:41:36 +0100

    commit

--- back/migrations/20250211095337_beneficiary_organisation.js
+++ back/migrations/20250211095337_beneficiary_organisation.js
@@ -8,10 +8,8 @@ FOREIGN KEY (beneficiary_id) REFERENCES beneficiary(id) ON DELETE CASCADE,
             FOREIGN KEY (organisation_id) REFERENCES organisation(id) ON DELETE CASCADE
             );
     `);
-    console.log("Table beneficiary_organisation créée avec succès !");
 }
 
 export async function down(client) {
     await client.execute(`DROP TABLE IF EXISTS beneficiary_organisation;`);
-    console.log("Table beneficiary_organisation supprimée !");
 }
--- back/seeds/type_organisation.js
+++ back/seeds/type_organisation.js
@@ -4,18 +4,18 @@
 * @returns { Promise<void> }
 */
 export async function seed(client) {
-//     await client.execute("DELETE FROM type_organisation;");
-//     await client.execute(`INSERT INTO type_organisation (id_organisation, id_type)
-// VALUES
-//     (1, 2),
-//     (2, 3),
-//     (3, 1),
-//     (4, 5),
-//     (5, 4),
-//     (6, 2),
-//     (7, 1),
-//     (8, 3),
-//     (9, 5),
-//     (10, 4)
-// `);
+    await client.execute("DELETE FROM type_organisation;");
+    await client.execute(`INSERT INTO type_organisation (id_organisation, id_type)
+VALUES
+    (1, 2),
+    (2, 3),
+    (3, 1),
+    (4, 5),
+    (5, 4),
+    (6, 2),
+    (7, 1),
+    (8, 3),
+    (9, 5),
+    (10, 4)
+`);
 }
