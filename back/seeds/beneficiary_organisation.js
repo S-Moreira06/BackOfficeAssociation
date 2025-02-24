555609b956153a9904818cdc5eb8f67496c3526a
Noura <nourallahbejaoui@gmail.com>
2025-02-24 15:39:39 +0100

    commit

--- back/seeds/beneficiary_organisation.js
+++ back/seeds/beneficiary_organisation.js
@@ -4,5 +4,18 @@
 * @returns { Promise<void> }
 */
 export async function seed(client) {
-  
-}
+    await client.execute("DELETE FROM beneficiary_organisation;");
+    await client.execute(`INSERT INTO beneficiary_organisation (organisation_id, beneficiary_id)
+VALUES
+    (1, 2),
+    (2, 3),
+    (3, 1),
+    (4, 5),
+    (5, 4),
+    (6, 2),
+    (7, 1),
+    (8, 3),
+    (9, 5),
+    (10, 4)
+`);
+    }
