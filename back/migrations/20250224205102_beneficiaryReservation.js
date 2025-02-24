ba790d4a8707cd188b0c7b42ddeaee7a9d0e9c98
Aïmen <aimen2nice@gmail.com>
2025-02-24 22:55:39 +0100

    migration ok

--- back/migrations/20250224205102_beneficiaryReservation.js
+++ back/migrations/20250224205102_beneficiaryReservation.js
@@ -6,13 +6,14 @@
 export async function up(client) {
     await client.execute(
         `CREATE TABLE IF NOT EXISTS beneficiary_reservation (
-                                                                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                                                id_beneficiary INTEGER NOT NULL,
-                                                                id_reservation INTEGER  NOT NULL,
-                                                                FOREIGN KEY (id_beneficiary) REFERENCES beneficiary(id)
-                                                                FOREIGN KEY (id_reservation) REFERENCES reservation(id)
+                                                          id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                                          id_beneficiary INTEGER NOT NULL,
+                                                          id_reservation INTEGER NOT NULL,
+                                                          FOREIGN KEY (id_beneficiary) REFERENCES beneficiary(id)
+                                                          FOREIGN KEY (id_reservation) REFERENCES reservation(id)
             )`);
 }
+
 /**
  * Migrates the database schema downward, making changes to roll the schema back to a previous version.
  * @param client - The libsql client to use when migrating.
