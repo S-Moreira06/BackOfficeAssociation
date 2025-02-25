6b18c0c3b1e0b2a4a73cdc8e762200e0bba6d6b9
Moreira_Stephane <moreira.s@outlook.com>
2025-02-25 11:09:29 +0100

    checkout

--- front/src/page/deleteUser.jsx
+++ front/src/page/deleteUser.jsx
@@ -1,16 +1,20 @@
+import { useLocation } from "react-router-dom";
+
 import { Button } from '@/components/ui/button'
+
 
 import { deleteUser } from '@/api/user';
 
 
-export default function deleteUser() {
-
-    const confirmDelete = ;
+export default function DeleteUser() {
+    const location = useLocation();
+    const userId = location.state?.userId; 
+    console.log(userId)
     return (
     <>
         <p>Etes vous sure de vouloir supprimer cet utilisateur?</p>
         <div>
-            <Button>Oui</Button>
+            <Button onClick={() => deleteUser(userId)}>oui</Button>
             <Button>Non</Button>
         </div>
     </>
