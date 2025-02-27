0e67b466ad15dbd1a0731a653ba01110a5db319a
Moreira_Stephane <moreira.s@outlook.com>
2025-02-27 13:37:01 +0100

    create user button on list

--- front/src/layout/UsersList.jsx
+++ front/src/layout/UsersList.jsx
@@ -41,54 +41,58 @@ export default function UsersList() {
         console.log("DATA", data)
     }, [data])
     return (
-        <Table>
-        <TableCaption className="caption-top">
-            Liste des utilisateurs
-        </TableCaption>
-        
-            <TableHeader>
-            <TableRow>
-                <TableHead>Nom</TableHead>
-                <TableHead>Prénom</TableHead>
-                <TableHead>Role</TableHead>
-                <TableHead>Téléphone</TableHead>
-                
-            </TableRow>
-            </TableHeader>
-            <TableBody>
-            {data?.users.length > 0 && data.users.map((user)=>{
-            return (
-                <TableRow key={user.id}>
-                    <TableCell>{user?.firstname}</TableCell>
-                    <TableCell>{user?.lastname}</TableCell>
-                    <TableCell>{user?.role}</TableCell>
-                    <TableCell>{user?.phone}</TableCell>
-                    <TableCell>{user?.is_archived}</TableCell>
-                    <TableCell>
-                        <Button onClick={() => navigate("/update-user",{ state: { userId: user.id }})}>Modifier</Button>
-                    </TableCell>
-                    <TableCell>
-                        <AlertDialog>
-                            <AlertDialogTrigger>Supprimer</AlertDialogTrigger>
-                            <AlertDialogContent className="bg-white">
-                                <AlertDialogHeader>
-                                <AlertDialogTitle>Etes vous sure de vouloir supprimer l'utilisateur?</AlertDialogTitle>
-                                <AlertDialogDescription>
-                                    Souhaitez vous désactiver le compte de cet utilisateur?
-                                </AlertDialogDescription>
-                                </AlertDialogHeader>
-                                <AlertDialogFooter>
-                                <AlertDialogCancel>Annuler</AlertDialogCancel>
-                                <AlertDialogAction onClick={() => mutation.mutate(user.id)}>Oui</AlertDialogAction>
-                                </AlertDialogFooter>
-                            </AlertDialogContent>
-                        </AlertDialog>
-                    </TableCell>
+        <>
+            <Button onClick={()=>navigate("/create-user")}>Créer un utilisateur</Button>
+            
+            <Table>
+            <TableCaption className="caption-top">
+                Liste des utilisateurs
+            </TableCaption>
+            
+                <TableHeader>
+                <TableRow>
+                    <TableHead>Nom</TableHead>
+                    <TableHead>Prénom</TableHead>
+                    <TableHead>Role</TableHead>
+                    <TableHead>Téléphone</TableHead>
+                    
                 </TableRow>
-            )
-            })}
-            </TableBody>
-        
-        </Table>
+                </TableHeader>
+                <TableBody>
+                {data?.users.length > 0 && data.users.map((user)=>{
+                return (
+                    <TableRow key={user.id}>
+                        <TableCell>{user?.firstname}</TableCell>
+                        <TableCell>{user?.lastname}</TableCell>
+                        <TableCell>{user?.role}</TableCell>
+                        <TableCell>{user?.phone}</TableCell>
+                        <TableCell>{user?.is_archived}</TableCell>
+                        <TableCell>
+                            <Button onClick={() => navigate("/update-user",{ state: { userId: user.id }})}>Modifier</Button>
+                        </TableCell>
+                        <TableCell>
+                            <AlertDialog>
+                                <AlertDialogTrigger>Supprimer</AlertDialogTrigger>
+                                <AlertDialogContent className="bg-white">
+                                    <AlertDialogHeader>
+                                    <AlertDialogTitle>Etes vous sure de vouloir supprimer l'utilisateur?</AlertDialogTitle>
+                                    <AlertDialogDescription>
+                                        Souhaitez vous désactiver le compte de cet utilisateur?
+                                    </AlertDialogDescription>
+                                    </AlertDialogHeader>
+                                    <AlertDialogFooter>
+                                    <AlertDialogCancel>Annuler</AlertDialogCancel>
+                                    <AlertDialogAction onClick={() => mutation.mutate(user.id)}>Oui</AlertDialogAction>
+                                    </AlertDialogFooter>
+                                </AlertDialogContent>
+                            </AlertDialog>
+                        </TableCell>
+                    </TableRow>
+                )
+                })}
+                </TableBody>
+            
+            </Table>
+        </>
     )
 }
