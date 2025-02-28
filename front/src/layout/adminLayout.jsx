a7e252610e523ff0d8a23286ad0287a7fd2fca7b
Moreira_Stephane <moreira.s@outlook.com>
2025-02-28 19:22:45 +0100

    add empty shadui sidebar

--- front/src/layout/adminLayout.jsx
+++ front/src/layout/adminLayout.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/private/Sidebar"
+
 import Footer from "@/components/private/Footer";
 import Header from "@/components/private/Header";
 import Guest from "@/page/guest";
 
-export default function AdminLayout() {
+export default function AdminLayout({ children }) {
     const storedData = localStorage.getItem("accessToken")
 
     return(
@@ -12,7 +15,14 @@ export default function AdminLayout() {
             {storedData ? (
             <>
                 <Header />
-                <Outlet/>
+                <SidebarProvider>
+                <AppSidebar />
+                <main>
+                    <SidebarTrigger />
+                    {children}
+                    <Outlet/>
+                </main>
+                </SidebarProvider>
                 <Footer/>
             </>
             ):(
