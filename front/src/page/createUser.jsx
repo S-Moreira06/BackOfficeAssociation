e665201245c2fe86317ada850a0c36fd428a82a2
Moreira_Stephane <moreira.s@outlook.com>
2025-02-28 10:50:05 +0100

    sorry modification faite avec sambeau sur dev

--- front/src/page/createUser.jsx
+++ front/src/page/createUser.jsx
@@ -27,7 +27,8 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import {  useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
