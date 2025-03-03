c3bd935b7ce77f841002573a17ba6f5a92c79490
Moreira_Stephane <moreira.s@outlook.com>
2025-03-03 23:17:51 +0100

    fix css and tailwind variables

--- front/src/page/private/requestDetail.jsx
+++ front/src/page/private/requestDetail.jsx
@@ -5,8 +5,9 @@ import {
     CardFooter,
     CardHeader,
     CardTitle,
-  } from "@/components/ui/card"
-  
+} from "@/components/ui/card"
+import { Separator } from "@/components/ui/separator"
+
 
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { useLocation, useNavigate } from "react-router-dom";
@@ -27,29 +28,29 @@ export default function RequestDetail () {
     console.log(data)
     return (
         <>
-            <Card className="mx-auto w-[80%]">
+            <Card className="mx-auto w-[80%] rounded-md">
                 <CardHeader>
                     <CardTitle>Requete n° {data?.request.id} <br/> <GetDate timestamp={data?.request.created_at}/></CardTitle>
                     <CardDescription>{data?.request.category}</CardDescription>
                 </CardHeader>
                 <CardContent className="mr-10 grid grid-cols-2">
-                    
                     <p>Nom : </p>
                     <p className="text-center">{data?.request.name}</p>
-                    
-                <p>Localisation : </p>
-                <div className="text-center">
-                    <p>{data?.request.address}</p> 
-                    <p>{data?.request.zip} </p>
-                    <p>{data?.request.city}</p>
-                </div>
-                <p>Contact :</p> 
-                <div className="text-center">
-                    <p>{data?.request.lastname} {data?.request.firstname}</p> 
-                    <p>{data?.request.phone} </p>
-                    <p>{data?.request.email} </p>
-                </div>
-                
+                    <Separator className="border"/><Separator className="border"/>
+
+                    <p>Localisation : </p>
+                    <div className="text-center">
+                        <p>{data?.request.address}</p> 
+                        <p>{data?.request.zip} </p>
+                        <p>{data?.request.city}</p>
+                    </div>
+                    <Separator className="border"/><Separator className="border"/>
+                    <p>Contact :</p> 
+                    <div className="text-center">
+                        <p>{data?.request.lastname} {data?.request.firstname}</p> 
+                        <p>{data?.request.phone} </p>
+                        <p>{data?.request.email} </p>
+                    </div>
                 </CardContent>
                 <CardFooter>
                     <p>Card Footer</p>
