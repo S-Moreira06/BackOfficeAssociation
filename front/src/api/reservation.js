58991e3792d451510d110622ad9cb3f1a9b0e071
Moreira_Stephane <moreira.s@outlook.com>
2025-03-11 18:44:38 +0100

    set api function, ready to test

--- front/src/api/reservation.js
+++ front/src/api/reservation.js
@@ -4,4 +4,35 @@ async function createReservation(data) {
     return await instance.post("/reservation",data)
 }
 
-export {createReservation}
+async function getAllReservationByAvailability(data) {
+    return await instance.post("/reservation",data)
+    try {
+        const response = await instance.get(`/reservation?id_availability=${data}`)
+        console.log(response)
+        return response.data
+    } catch (error) {
+        return error
+    }
+}
+
+async function getAllReservation() {
+    try {
+        const response = await instance.get("/reservation")
+        console.log(response)
+        return response.data
+    } catch (error) {
+        return error
+    }
+}
+
+async function getReservationById(id) {
+    try {
+        const response = await instance.get(`/reservation/${id}`)
+        console.log(response)
+        return response.data
+    } catch (error) {
+        return error
+    }
+}
+
+export {createReservation, getAllReservationByAvailability, getAllReservation, getReservationById}
