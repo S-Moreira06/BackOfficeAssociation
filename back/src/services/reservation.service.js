0970fd9aeea877052251e787d045d608fdcaa58f
Moreira_Stephane <moreira.s@outlook.com>
2025-03-13 21:14:01 +0100

    set innerjoin to get asso name with availability

--- back/src/services/reservation.service.js
+++ back/src/services/reservation.service.js
@@ -57,9 +57,15 @@ async function getTotalByName(name) {
 }
 
 async function getAllReservationByAvailability(id_availability) {
-  const query = 'SELECT * FROM reservation WHERE id_availability = ?';
+  const query = `
+    SELECT r.*, o.name AS organisation_name
+    FROM reservation r
+    INNER JOIN organisation o ON r.id_organisation = o.id
+    WHERE r.id_availability = ?;
+  `;
+
   const result = await db.prepare(query).all(id_availability);
-  console.log('Type de id_availability:', typeof id_availability, id_availability);
+  console.log('resultat:', result);
   return result;
 }
 async function valid(id_reservation) {
