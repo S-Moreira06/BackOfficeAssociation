b49e4de27b0e4c872a144461583ec3062756cd2a
Moreira_Stephane <moreira.s@outlook.com>
2025-03-24 08:50:39 +0100

    navbar collapsible offcanvas

--- front/src/components/private/Sidebar.jsx
+++ front/src/components/private/Sidebar.jsx
@@ -9,51 +9,69 @@ import {
     SidebarMenu,
     SidebarMenuButton,
     SidebarMenuItem,
+    SidebarRail
   } from "@/components/ui/sidebar"
+  import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+  } from "@/components/ui/collapsible"
+  
 import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router";
+import { ChevronDown } from 'lucide-react';
+
+
 
 export function AppSidebar() {
 
     const menuItems = [
-        { title: "ADMIN", links: [{ name: "Liste des Utilisateurs", path: "/user-list" }, { name: "Créer un utilisateur", path: "/create-user" },{ name: "Liste des restaurants", path: "/restaurant-list" }, { name: "Créer un restaurant", path: "/create-restaurant" },{ name: "Liste des associations", path: "/association-list" }, { name: "Créer une association", path: "/create-association" }] },
-        { title: "Restaurants", links: [{ name: "Créer une disponibilité", path: "/create-availability" }, {name: "Mes disponibilités"},{name: "Mes réservations"}, {name: "Profil du restaurant"}] },
-        { title: "Associations", links: [{ name: "Liste des bénéficiaires", path: "/beneficiary-list" }, { name: "Créer un bénéficiaire", path: "/create-beneficiary" },{ name: "Liste des disponibilités", path: "/availability-list" }, {name: "Mes agents de réservation"},{name: "Mes disponibilités"}] },
-        // { title: "Disponibilités", links: [{ name: "Liste des disponibilités", path: "/availability-list" }, { name: "Créer une disponibilité", path: "/create-availability" }] },
-        // { title: "Réservations", links: [{ name: "Liste des réservations", path: "/reservation-list" }, { name: "Créer une réservation", path: "/create-reservation" }] },
+        { logo:"ChevronDown",title: "ADMIN", links: [{ name: "Liste des Utilisateurs", path: "/user-list" }, { name: "Créer un utilisateur", path: "/create-user" },{ name: "Liste des restaurants", path: "/restaurant-list" }, { name: "Créer un restaurant", path: "/create-restaurant" },{ name: "Liste des associations", path: "/association-list" }, { name: "Créer une association", path: "/create-association" }] },
+        { logo:"ChevronDown",title: "Restaurants", links: [{ name: "Créer une disponibilité", path: "/create-availability" }, {name: "Mes disponibilités"},{name: "Mes réservations"}, {name: "Profil du restaurant"}] },
+        { logo:"ChevronDown",title: "Associations", links: [{ name: "Liste des bénéficiaires", path: "/beneficiary-list" }, { name: "Créer un bénéficiaire", path: "/create-beneficiary" },{ name: "Liste des disponibilités", path: "/availability-list" }, {name: "Mes agents de réservation"},{name: "Mes disponibilités"}] },
     ];
     const navigate = useNavigate()
 
     return (
-        <Sidebar>
+        <Sidebar collapsible="offcanvas">
             <SidebarHeader />
             <SidebarContent className="gap-0">
-
               {menuItems.map((item) => (
+                <Collapsible defaultOpen className="group/collapsible">
                 <SidebarGroup key={item.title}>
-                    <SidebarGroupLabel  >{item.title}</SidebarGroupLabel>
-                    <SidebarGroupContent> 
-                        <SidebarMenu>
-                            <SidebarMenuItem >
-                                {item.links.map((link) => (
-                                <SidebarMenuButton key={link.name}>
-                                            <li >
-                                                <div>
-                                                    <p onClick={() => navigate(link.path)} className="">
-                                                        {link.name}
-                                                    </p>
-                                                </div>
-                                            </li>
-                                </SidebarMenuButton>
-                            ))}
-                            </SidebarMenuItem>
-                        </SidebarMenu>
-                    </SidebarGroupContent>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-shield-user"><path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z"/><path d="M6.376 18.91a6 6 0 0 1 11.249.003"/><circle cx="12" cy="11" r="4"/></svg>
+                    <SidebarGroupLabel asChild >
+                        <CollapsibleTrigger>
+                        {item.title}
+                            <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                        </CollapsibleTrigger>
+                    </SidebarGroupLabel>
+                    <CollapsibleContent>
+                        <SidebarGroupContent> 
+                            <SidebarMenu>
+                                <SidebarMenuItem >
+                                    {item.links.map((link) => (
+                                    <SidebarMenuButton key={link.name}>
+                                                <li >
+                                                    <div>
+                                                        <p onClick={() => navigate(link.path)} className="">
+                                                            {link.name}
+                                                        </p>
+                                                    </div>
+                                                </li>
+                                    </SidebarMenuButton>
+                                ))}
+                                </SidebarMenuItem>
+                            </SidebarMenu>
+                        </SidebarGroupContent>                    
+                    </CollapsibleContent>
                 </SidebarGroup>
+                </Collapsible>
                 ))}
 
             </SidebarContent>
-            <SidebarFooter />
+            <SidebarFooter>Ctrl+b to open/close</SidebarFooter>
+             {/* <SidebarRail /> */}
         </Sidebar>
     )
 }
