5a23369f62a5958f2baee56287c74a00f8f8e821
Moreira_Stephane <moreira.s@outlook.com>
2025-03-24 23:51:54 +0100

    ajour tri user

--- front/src/page/private/userList.jsx
+++ front/src/page/private/userList.jsx
@@ -1,5 +1,5 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router";
 
 import { Button } from '@/components/ui/button'
@@ -22,13 +22,13 @@ import {
     AlertDialogHeader,
     AlertDialogTitle,
     AlertDialogTrigger,
-  } from "@/components/ui/alert-dialog"
+} from "@/components/ui/alert-dialog"
 
 import { deleteUser, getAllUser } from '@/api/user';
 
 export default function UserList() {
-    const { isPending, isError, data, error } = useQuery({ queryKey: ['userList'], queryFn: getAllUser })
-    const navigate = useNavigate()
+    const { isPending, isError, data, error } = useQuery({ queryKey: ['userList'], queryFn: getAllUser });
+    const navigate = useNavigate();
     const queryClient = useQueryClient();
 
     const mutation = useMutation({
@@ -37,62 +37,95 @@ export default function UserList() {
             queryClient.invalidateQueries(['userList']); 
         },
     });
-    useEffect(()=>{
-        console.log("DATA", data)
-    }, [data])
+
+    const [sortConfig, setSortConfig] = useState({ key: '', direction: 'asc' });
+
+    useEffect(() => {
+        console.log("DATA", data);
+    }, [data]);
+
+    const sortData = (data, key, direction) => {
+        return data?.users.sort((a, b) => {
+            if (a[key] < b[key]) {
+                return direction === 'asc' ? -1 : 1;
+            }
+            if (a[key] > b[key]) {
+                return direction === 'asc' ? 1 : -1;
+            }
+            return 0;
+        });
+    };
+
+    const handleSort = (key) => {
+        let direction = 'asc';
+        if (sortConfig.key === key && sortConfig.direction === 'asc') {
+            direction = 'desc';
+        }
+        setSortConfig({ key, direction });
+    };
+
+    const sortedData = sortData(data, sortConfig.key, sortConfig.direction);
+
     return (
         <>
-            <Button variant="outline" className="mt-2" onClick={()=>navigate("/create-user")}>Créer un utilisateur</Button>
+            <Button variant="outline" className="mt-2" onClick={() => navigate("/create-user")}>Créer un utilisateur</Button>
             
             <Table>
-            <TableCaption className="caption-top text-xl">
-                Liste des utilisateurs
-            </TableCaption>
-            
+                <TableCaption className="caption-top text-xl">
+                    Liste des utilisateurs
+                </TableCaption>
+
                 <TableHeader>
-                <TableRow>
-                    <TableHead>Nom</TableHead>
-                    <TableHead>Prénom</TableHead>
-                    <TableHead>Role</TableHead>
-                    <TableHead>Téléphone</TableHead>
-                    
-                </TableRow>
-                </TableHeader>
-                <TableBody>
-                {data?.users.length > 0 && data.users.map((user)=>{
-                return (
-                    <TableRow key={user.id}>
-                        <TableCell>{user?.firstname}</TableCell>
-                        <TableCell>{user?.lastname}</TableCell>
-                        <TableCell>{user?.role}</TableCell>
-                        <TableCell>{user?.phone}</TableCell>
-                        <TableCell>{user?.is_archived}</TableCell>
-                        <TableCell>
-                            <Button onClick={() => navigate("/update-user",{ state: { userId: user.id }})}>Modifier</Button>
-                        </TableCell>
-                        <TableCell>
-                            <AlertDialog>
-                                <AlertDialogTrigger>Supprimer</AlertDialogTrigger>
-                                <AlertDialogContent className="bg-white">
-                                    <AlertDialogHeader>
-                                    <AlertDialogTitle>Etes vous sure de vouloir supprimer l'utilisateur?</AlertDialogTitle>
-                                    <AlertDialogDescription>
-                                        Souhaitez vous désactiver le compte de cet utilisateur?
-                                    </AlertDialogDescription>
-                                    </AlertDialogHeader>
-                                    <AlertDialogFooter>
-                                    <AlertDialogCancel>Annuler</AlertDialogCancel>
-                                    <AlertDialogAction onClick={() => mutation.mutate(user.id)}>Oui</AlertDialogAction>
-                                    </AlertDialogFooter>
-                                </AlertDialogContent>
-                            </AlertDialog>
-                        </TableCell>
+                    <TableRow>
+                        <TableHead onClick={() => handleSort('firstname')}>
+                            Nom {sortConfig.key === 'firstname' ? (sortConfig.direction === 'asc' ? '↑' : '↓') : ''}
+                        </TableHead>
+                        <TableHead onClick={() => handleSort('lastname')}>
+                            Prénom {sortConfig.key === 'lastname' ? (sortConfig.direction === 'asc' ? '↑' : '↓') : ''}
+                        </TableHead>
+                        <TableHead onClick={() => handleSort('role')}>
+                            Role {sortConfig.key === 'role' ? (sortConfig.direction === 'asc' ? '↑' : '↓') : ''}
+                        </TableHead>
+                        <TableHead onClick={() => handleSort('phone')}>
+                            Téléphone {sortConfig.key === 'phone' ? (sortConfig.direction === 'asc' ? '↑' : '↓') : ''}
+                        </TableHead>
                     </TableRow>
-                )
-                })}
+                </TableHeader>
+
+                <TableBody>
+                    {sortedData?.length > 0 && sortedData.map((user) => {
+                        return (
+                            <TableRow key={user.id}>
+                                <TableCell>{user?.firstname}</TableCell>
+                                <TableCell>{user?.lastname}</TableCell>
+                                <TableCell>{user?.role}</TableCell>
+                                <TableCell>{user?.phone}</TableCell>
+                                <TableCell>{user?.is_archived}</TableCell>
+                                <TableCell>
+                                    <Button onClick={() => navigate("/update-user", { state: { userId: user.id } })}>Modifier</Button>
+                                </TableCell>
+                                <TableCell>
+                                    <AlertDialog>
+                                        <AlertDialogTrigger>Supprimer</AlertDialogTrigger>
+                                        <AlertDialogContent className="bg-white">
+                                            <AlertDialogHeader>
+                                                <AlertDialogTitle>Etes-vous sûr de vouloir supprimer l'utilisateur ?</AlertDialogTitle>
+                                                <AlertDialogDescription>
+                                                    Souhaitez-vous désactiver le compte de cet utilisateur ?
+                                                </AlertDialogDescription>
+                                            </AlertDialogHeader>
+                                            <AlertDialogFooter>
+                                                <AlertDialogCancel>Annuler</AlertDialogCancel>
+                                                <AlertDialogAction onClick={() => mutation.mutate(user.id)}>Oui</AlertDialogAction>
+                                            </AlertDialogFooter>
+                                        </AlertDialogContent>
+                                    </AlertDialog>
+                                </TableCell>
+                            </TableRow>
+                        );
+                    })}
                 </TableBody>
-            
             </Table>
         </>
-    )
+    );
 }
